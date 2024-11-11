0b4eb5d87156d9eeb3ae9dcc7690b78d36350cb0
Vito Di Villa <vitodivilla@gmail.com>
2024-11-12 00:21:03 +0100

    initial commit

--- example/main_test.go
+++ example/main_test.go
@@ -10,18 +10,16 @@ var (
 	result []byte
 )
 
-func init() {
-}
-
 func Benchmark_Render(b *testing.B) {
 	var data, r []byte
 	data, _ = sjson.SetBytes(data, `title`, `Hello Bulma!`)
 	data, _ = sjson.SetBytes(data, `nums`, []int{1, 2, 3, 4, 5, 6})
 	data, _ = sjson.SetBytes(data, `show`, true)
-	view := NewView()
 
 	for n := 0; n < b.N; n++ {
+		view := NewView()
 		r = view.Run(data)
+		view.Close()
 	}
 
 	result = r
